141d511a87ec03d050dbbbffc7fdbcd417afaf32
Gabriel Andrade <gabriel.santana@gmail.com>
2022-08-30 06:26:22 -0300

    Pass to first test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  request.user = user;
+
+  next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
